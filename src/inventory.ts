4e2ae7bd618b2f4b5c5eb1d5b543005527e8e6a5
james <jamesedwarddevine@gmail.com>
2021-12-16 11:04:54 -0500

    added tests and all passing

--- src/inventory.ts
+++ src/inventory.ts
@@ -1,6 +1,6 @@
 import {Product} from "./products"
 
-interface InventoryItem {
+export interface InventoryItem {
     product: Product;
     quantity: number;
 }
@@ -11,7 +11,7 @@ let inventory: InventoryItem[] = [
     {product:{name: "LED", price: 1.00}, quantity:20},
 ];
 
-const calcInventoryValue = (array: InventoryItem[]): number => {
+export const calcInventoryValue = (array: InventoryItem[]): number => {
     let total:number = 0;
     for(let i = 0; i < array.length; i++) {
         total = total + (array[i].quantity*array[i].product.price)
--- src/products.ts
+++ src/products.ts
@@ -6,9 +6,15 @@ export interface Product {
 let products:Array<Product> = [{name: "Tacos", price: 5}, {name: "Potatoes", price: 10}, {name: "CornDogs", price: 15}];
 
 export const calcAverageProductPrice = (arrayOfProducts:Array<Product>):number => {
-   let average:number = arrayOfProducts.reduce((acc,product) => acc + product.price, 0) / arrayOfProducts.length;
-   console.log(average);
-   return average;
+   if(arrayOfProducts.length>0){
+       let average:number = arrayOfProducts.reduce((acc,product) => acc + product.price, 0) / arrayOfProducts.length;
+    console.log(average);
+    return average;
+    } else {
+        let average = 0;
+        return average;
+    }
 }
+
 
 calcAverageProductPrice(products)
